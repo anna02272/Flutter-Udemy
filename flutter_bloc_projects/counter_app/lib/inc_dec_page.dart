a3bdcf613de832ef8203c8263735c2cc7a07d18a
anna02272 <domonjianna@gmail.com>
2024-04-08 11:40:58 +0200

    Flutter bloc tutorials

---
+++ flutter_bloc_projects/counter_app/lib/inc_dec_page.dart
@@ -0,0 +1,35 @@
+import 'package:counter_app/bloc/counter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class IncDecPage extends StatelessWidget {
+  const IncDecPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterIncremented());
+              // counterCubit.increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterDecremented());
+              // counterCubit.decrement();
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_bloc_projects/infogames/android/app/src/main/kotlin/com/example/infogames/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.infogames
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
